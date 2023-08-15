9e97e97cd55537531a3eabdcf021886b4758d96b
Dmitriy <119503758+koshcheyTD@users.noreply.github.com>
2023-08-15 17:37:41 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*scroll_menu*/
+$(document).ready(function() {
+    $('nav a[href^="#"]').click(function(){
+        let target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        },500);
+        $('nav a[href^="#"]').parent().removeClass('active');
+        $(this).parent().addClass('active');
+        $('.menu__mobile .menu').toggle(500);
+        $('.menu__burger').toggleClass('close');
+        return false;
+    });
+    /*burger_menu*/
+    $('.menu__burger').click(function(){
+        $('.menu__mobile .menu').toggle(500);
+        $(this).toggleClass('close');
+    })
+});
+
